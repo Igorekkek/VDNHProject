e3b137d65b10f3d8df76fb4ee7dc0f9e5885fa25
Rusandor <ruslanpalkin44@gmail.com>
2023-02-06 01:02:53 +0300

    feature/made history work

--- frontend/src/components/HistoryList/HistoryList.jsx
+++ frontend/src/components/HistoryList/HistoryList.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useHistory } from '../../hooks'
 import cl from './HistoryList.module.css'
+import { MapContext } from '../../context/MapContext'
 
 const HistoryList = () => {
   const { isLoading, error, data } = useHistory()
+  const { setCurRefPoints } = useContext(MapContext)
 
   if (isLoading) return <ul className={cl.routes}></ul>
   if (error) return <div>Error occurred</div>
@@ -11,9 +13,16 @@ const HistoryList = () => {
   return (
     <ul className={cl.routes}>
       {data?.post?.length && data.post.map(route => {
-        return <ol className={cl.route} style={{backgroundColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`}}>
-          {route.map((point) => <li className={cl.route__item}><h4>{point.title}</h4>  </li>)}
-          </ol>
+        return <ol className={cl.route}
+                   style={{ backgroundColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})` }}
+                   onClick={() => {
+                     setCurRefPoints(route)
+                   }}
+      >
+          {route.map((point) => <li className={cl.route__item}>
+            <h4 className={cl.route__title}>{point.title}</h4>
+          </li>)}
+        </ol>
       })}
     </ul>
   )
