d20a933b060ad6c9b4e76b2e980b8cff82065b0c
Rusandor <ruslanpalkin44@gmail.com>
2023-02-05 12:52:54 +0300

    fix/added title field to POI

--- backend/serverAPI/serializers.py
+++ backend/serverAPI/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class POISerializer(serializers.ModelSerializer):
     class Meta:
         model = PointOfInterest
-        fields = ('code', 'category', "longitude", "latitude")
+        fields = ('code', 'category', 'title', "longitude", "latitude")
