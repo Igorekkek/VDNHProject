7881e8d8b1deb3d55629e1f14ca4329bfcd99a06
Rusandor <ruslanpalkin44@gmail.com>
2023-02-09 20:36:06 +0300

    feat/I honestly dunno, I made those changes 3 days ago and now got no clue bout em

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,16 @@
 import './App.css'
-import CustomMap from './components/CustomMap/CustomMap'
-import RouteSelector from './components/RouteSelector/RouteSelector'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { MapProvider } from './context/MapContext'
-import { RouteView } from './components/RouteView/RouteView'
-import MapButton from './components/MapButton/MapButton'
+import MapPage from './pages/MapPage/MapPage'
 
 const queryClient = new QueryClient()
-const saveRoute = route => {
-  return fetch('http://localhost:8000/api/addHistory/', {
-    method: 'POST', body: JSON.stringify({ data: route, user_code: localStorage.getItem('userMapApiCode') }), headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(res => {
-      if (!res.ok) throw new Error(`Error ${res.status}`)
-      return res.json()
-    }).catch(console.error)
-}
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <main className="App">
         <MapProvider>
-          <div className="map">
-            <div className="map__content">
-              <CustomMap/>
-            </div>
-            <div className="map__point-list">
-              <RouteSelector/>
-            </div>
-          </div>
-          <div className="route-view">
-            <RouteView/>
-          </div>
-          <div className="map__buttons">
-            {/*TODO: Make validation + call useQuery of 'getHistory' and give it mocked data instead of fetch to update it immediately in history window ?*/}
-            <MapButton text={'Сохранить маршрут'}
-                       onClick={async mapContext => {
-                         await saveRoute(mapContext.curRefPoints)
-                       }}
-            />
-
-            <MapButton text={'Сбросить маршрут'}
-                       onClick={mapContext => {
-                         mapContext.setCurRefPoints([])
-                       }}
-            />
-          </div>
+          <MapPage/>
         </MapProvider>
       </main>
     </QueryClientProvider>
---
+++ frontend/src/pages/MapPage/MapPage.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react'
+import cl from './MapPage.module.css'
+import CustomMap from '../../components/CustomMap/CustomMap'
+import RouteSelector from '../../components/RouteSelector/RouteSelector'
+import { RouteView } from '../../components/RouteView/RouteView'
+import { MapContext } from '../../context/MapContext'
+
+const saveRoute = route => {
+  return fetch('http://localhost:8000/api/addHistory/', {
+    method: 'POST', body: JSON.stringify({ data: route, user_code: localStorage.getItem('userMapApiCode') }), headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if (!res.ok) throw new Error(`Error ${res.status}`)
+      return res.json()
+    }).catch(console.error)
+}
+
+const MapPage = () => {
+  const { curRefPoints, setCurRefPoints } = useContext(MapContext)
+
+  return (
+    <main className={cl.map}>
+      <div className={cl.map__content}>
+        <CustomMap/>
+      </div>
+      <div className={cl.map__pointList}>
+        <RouteSelector/>
+      </div>
+      <div className={cl.routeView}>
+        <RouteView/>
+      </div>
+      <div className={cl.map__buttons}>
+        {/*TODO: Make validation + call useQuery of 'getHistory' and give it mocked data instead of fetch to update it immediately in history window ?*/}
+        <button className={cl.map__button}
+          onClick={async mapContext => {
+            await saveRoute(curRefPoints)
+          }}>
+          Сохранить маршрут
+        </button>
+
+        <button className={cl.map__button}
+          onClick={mapContext => {
+            setCurRefPoints([])
+          }}>
+          Сбросить маршрут
+        </button>
+      </div>
+    </main>
+  )
+}
+
+export default MapPage
