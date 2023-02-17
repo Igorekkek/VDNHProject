6b5ffb0a98570f90dd55341159ce1f75a1b93c61
nf <rus>
2023-02-17 13:09:19 +0300

    routeList change

--- frontend/src/components/RouteList/RouteList.jsx
+++ frontend/src/components/RouteList/RouteList.jsx
@@ -2,19 +2,14 @@ import React, { useContext } from 'react'
 import { useHistory } from '../../hooks'
 import cl from './HistoryList.module.css'
 import { MapContext } from '../../context/MapContext'
-const HistoryList = () => {
-  const { isLoading, error, data } = useHistory()
-  const { setCurRefPoints, makeRouteEvent } = useContext(MapContext)
+const RouteList = ({routes}) => {
+  const {setCurRefPoints, makeRouteEvent} = useContext(MapContext)
   let formatter = new Intl.DateTimeFormat('ru', {minute: 'numeric', second: 'numeric'})
 
 
-  if (isLoading) return <ul className={cl.routes}></ul>
-  if (error) return <div>Error occurred</div>
-  if (!data?.post?.length) return null
-
   return (
     <ul className={cl.routes}>
-      {data.post.map(route => {
+      {routes.map(route => {
         const date = new Date(1970, 0, 1)
         date.setSeconds(route.time)
         const t = formatter.format(date)
@@ -36,4 +31,4 @@ const HistoryList = () => {
   )
 }
 
-export default HistoryList
+export default RouteList
--- frontend/src/components/RouteSelector/RouteSelector.jsx
+++ frontend/src/components/RouteSelector/RouteSelector.jsx
@@ -2,22 +2,35 @@ import React from 'react'
 import cl from './RouteSelector.module.css'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
 import { PointList } from '../PointList/PointList'
-import HistoryList from '../HistoryList/HistoryList'
+import RouteList from '../RouteList/RouteList'
+import { useHistory } from '../../hooks'
+import { useContext } from 'react'
+import { MapContext } from '../../context/MapContext'
 
 const RouteSelector = () =>
-  (
-    <Tabs className={cl.tabs}>
-      <TabList className={cl.tabList}>
-        <Tab className={cl.tabList__tab}>Маршрут</Tab>
-        <Tab className={cl.tabList__tab}>История</Tab>
-      </TabList>
-      <TabPanel>
-        <PointList/>
-      </TabPanel>
-      <TabPanel>
-        <HistoryList/>
-      </TabPanel>
-    </Tabs>
-  )
+  {
+    const { isLoading, error, data } = useHistory()
+    const { setCurRefPoints, makeRouteEvent } = useContext(MapContext)
+
+
+    return (
+      <Tabs className={cl.tabs}>
+        <TabList className={cl.tabList}>
+          <Tab className={cl.tabList__tab}>Маршрут</Tab>
+          <Tab className={cl.tabList__tab}>История</Tab>
+          <Tab className={cl.tabList__tab}>Готовые маршруты</Tab>
+        </TabList>
+        <TabPanel>
+          <PointList />
+        </TabPanel>
+        <TabPanel>
+          {data?.post.length !== 0 && <RouteList routes={data.post}/>}
+        </TabPanel>
+        <TabPanel>
+          {/* TODO: Static routes */}
+        </TabPanel>
+      </Tabs>
+    )
+  }
 
 export default RouteSelector
